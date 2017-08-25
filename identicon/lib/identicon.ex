b72aa9357e39460d4b978091785ce8e32b6b3de0
Jonathan <jonathan.wylliem@jurnal.id>
2017-08-25 09:39:39 +0700

    Introduction to struct

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -6,9 +6,9 @@ defmodule Identicon do
 
   # [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
   def hash_input(input) do
-     :crypto.hash(:md5, input)
+     hex = :crypto.hash(:md5, input)
      |> :binary.bin_to_list
+
+     %Identicon.Image{hex: hex}
   end
-
-
 end
