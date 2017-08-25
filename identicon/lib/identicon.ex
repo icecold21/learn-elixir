b99a376cf18f8dd1e7c776e9f5e1f674d919f7b0
Jonathan <jonathan.wylliem@jurnal.id>
2017-08-25 09:26:43 +0700

    Lesson 38 - Identicon generator

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,18 +1,14 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
   end
+
+  # [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
+  def hash_input(input) do
+     :crypto.hash(:md5, input)
+     |> :binary.bin_to_list
+  end
+
+
 end
